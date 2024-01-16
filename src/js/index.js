024c6b0f45b08ac281b51b4d0d5ebb9500286888
Shaginur <shaginu@gmail.com>
2024-01-16 07:02:32 +0500

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,67 @@
-import { getNumberOfGrades, getFirstGrade, getLastGrade } from "./stats.js";
+import { getNumberOfGrades, getFirstGrade, getLastGrade, getAverageGrade } from './stats.js'
 
-const gradesForm = document.querySelector("#grades-form");
-const yourGrade = document.querySelector("#your-grade");
+const gradesForm = document.querySelector('#grades-form')
+const yourGrade = document.querySelector('#your-grade')
+const sortSelect = document.querySelector('#sort')
 
-const grades = [14, 9, 13, 15, 18];
+const grades = [14, 9, 13, 15, 18]
 
 function render(grades) {
-  const tbody = document.querySelector("#stats-table tbody");
+  const tbody = document.querySelector('#stats-table tbody')
+  const gradeList = document.querySelector('#grade-list')
+  if (tbody && gradeList) {
+    tbody.innerHTML = `<tr>
+  <td>${getNumberOfGrades(grades) ?? 'No data'}</td>
+  <td>${getFirstGrade(grades) ?? 'No data'}</td>
+  <td>${getLastGrade(grades) ?? 'No data'}</td>
+  <td>${getAverageGrade(grades) ?? 'No data'}</td>
+  </tr>`
 
-  tbody.innerHTML = `<tr>
-  <td>${getNumberOfGrades(grades)}</td>
-  <td>${getFirstGrade(grades)}</td>
-  <td>${getLastGrade(grades)}</td>
-  </tr>`;
+    gradeList.innerHTML = ''
+    grades.forEach((grade, index) => {
+      const li = document.createElement('Li')
+      li.innerHTML = `
+      <span class="grade">${grade}</span>
+        <i class="fas fa-trash delete-icon" data-index="${index}"></i>
+    `
+      gradeList.appendChild(li)
+    })
+  }
 }
 
-gradesForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+const gradeList = document.querySelector('#grade-list')
+if (gradeList) {
+  gradeList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete-icon')) {
+      const indexToDelete = event.target.getAttribute('data-index')
+      if (indexToDelete !== null) {
+        grades.splice(indexToDelete, 1)
+        showNotification('Grade deleted successfully')
+        render(grades)
+      }
+    }
+  })
+}
+if (gradesForm) {
+  gradesForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let newGrade = Number.parseInt(yourGrade.value, 10)
+    grades.push(newGrade)
+    yourGrade.value = ''
+    showNotification('Grade added successfully')
+    render(grades)
+  })
+}
 
-  const newGrade = Number.parseInt(yourGrade.value, 10);
+export function showNotification(message) {
+  const notification = document.querySelector('#notification')
+  if (notification) {
+    notification.textContent = message
+    notification.style.display = 'block'
+    setTimeout(() => {
+      notification.style.display = 'none'
+    }, 1500)
+  }
+}
 
-  grades.push(newGrade);
-  yourGrade.value = "";
-  render(grades);
-});
-
-render(grades);
+render(grades)
--- src/js/stats.js
+++ src/js/stats.js
@@ -2,19 +2,31 @@
  * @param {array} grades
  */
 export function getNumberOfGrades(grades) {
-  // your code
+  const nomberLeng = grades.length || 0
+  return nomberLeng
 }
 
 /*
  * @param {array} grades
  */
 export function getFirstGrade(grades) {
-  // your code
+  const nomberFirst = grades[0]
+  return nomberFirst || 0
 }
 
 /*
  * @param {array} grades
  */
 export function getLastGrade(grades) {
-  // your code
+  const nomberLast = grades.at(-1)
+  return nomberLast || 0
 }
+
+export function getAverageGrade(grades) {
+  const [a, b, c, d, f] = grades
+  const average = (a + b + c + d + f) / grades.length
+  return average || 0
+}
+
+// Домашку зделала сам и пкрвое дополнительное задания
+//Остольное копировал кусками у вас так чтобы понять что для чего и как работает
